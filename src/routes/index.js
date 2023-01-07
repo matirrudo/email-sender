9dc87d05dc2314aec01faee5f5e45c51e3fe58e2
matirrudo <matirrudo@gmail.com>
2023-01-07 14:03:55 -0300

    Creacion del email sender

---
+++ src/routes/index.js
@@ -0,0 +1,39 @@
+const { Router } = require('express');
+const nodemailer = require('nodemailer');
+const router = Router();
+
+router.post('/send-message-form', async (req, res) => {
+    const {name, email, message} = req.body;
+
+    contentHTML = `
+        <h1>User Information</h1>
+        <ul>
+            <li>Name: ${name}</li>
+            <li>Email: ${email}</li>
+        </ul>
+        <p>${message}</p>
+    `;
+    console.log(contentHTML);
+    res.send({message: 'Message received', data: req.body});
+    const transporter = nodemailer.createTransport({
+        host:'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'horaios.contact@gmail.com',
+            pass: 'hicyyxuppbvvcuvs'
+        }
+    });
+
+    const info = await transporter.sendMail({
+        from: "'Horaios Server' <horaios.contact@gmail.com",
+        to: 'matirrudo@gmail.com',
+        subject: 'Website contact form',
+        html: contentHTML
+    });
+
+    console.log('Message sent', info.messageId);
+    
+})
+
+module.exports = router;
